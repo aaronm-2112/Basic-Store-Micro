992d59cf5e17e72abc55a66e542038bab6c43f27
aaronm-2112 <aaron.marroquin96@gmail.com>
2021-04-07 11:17:33 -0700

    Small change to logging in input-validation middleware

--- auth/src/middlewares/input-validation.ts
+++ auth/src/middlewares/input-validation.ts
@@ -6,11 +6,6 @@ export function validateInput(req: Request, res: Response, next: NextFunction) {
   // check if validation passes
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    //  if not send back a 400
-    errors.array().forEach((element) => {
-      console.error(element);
-    }); // changes listen
-
     throw new InputValidationError(errors);
   }
 
