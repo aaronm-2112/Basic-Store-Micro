944e8087a4c51da0564cda7e0a86479cd49e1536
aaronm-2112 <aaron.marroquin96@gmail.com>
2021-04-20 13:46:25 -0700

    More product repository tests and methods. TODO: Refactor implementations once more of the common code elements from multiple services begin being repeated.

--- products/src/repos/__test__/products-repo.test.ts
+++ products/src/repos/__test__/products-repo.test.ts
@@ -32,17 +32,23 @@ it("Inserts a product into the database successfully", async () => {
     },
   };
 
-  // // crceate the products repository
-  // let pr: ProductsRepo = new ProductsRepo();
-
   // insert the product into the repository
   let id = await pr.create(pm);
 
   // find the document in the repository using the id
   let productFromId = await pr.findOne(id);
 
+  expect(productFromId).not.toBe(undefined);
+
   // assert that the found document and the inserted document match
-  expect(productFromId).toEqual(pm);
+  expect(productFromId!.description).toEqual(pm.description);
+  expect(productFromId!.category).toEqual(pm.category);
+  expect(productFromId!.name).toEqual(pm.name);
+  expect(productFromId!.price).toEqual(pm.price);
+  expect(productFromId!.imageURI).toEqual(pm.imageURI);
+  expect(productFromId!.quantity).toEqual(pm.quantity);
+  expect(productFromId!.user.email).toEqual(pm.user.email);
+  expect(productFromId!.user.username).toEqual(pm.user.username);
 });
 
 it("Fails to insert a product with a negative price", async (done) => {
@@ -99,57 +105,173 @@ it("Decrease the quantity of a product found by its id", async () => {
   let pid = await pr.create(p);
 
   // decrement the product's quantity by calling orderCreated
-  await pr.orderCreated(pid);
+  await pr.changeQuantity(pid, false);
 
   // retrieve the updated product from the database
   let updatedP = await pr.findOne(pid);
 
   // check that the product's quantity is 0
-  expect(updatedP.quantity).toBe(0);
+  expect(updatedP!.quantity).toBe(0);
 });
 
 // Method: orderCancelled
 // used by the order cancelled event listener -- will eventually add a version number to this test for optimistic concurrency
 it("Increases the quantity of a product by its id", async () => {
   // insert a product into the database with a quantity of 1
+  let p: ProductModel = {
+    name: "Comb",
+    price: 12.5,
+    quantity: 1,
+    description: "A basic comb for hair.",
+    category: ["hair care"],
+    imageURI: "/path/to/comb/image",
+    user: {
+      username: "Sears",
+      email: "Searsshipping@gmail.com",
+    },
+  };
+
+  let pid = await pr.create(p);
+
   // increment the product's quantity by calling orderCancelled
+  await pr.changeQuantity(pid, true);
+
   // retrieve the updated product from the database
+  let updatedP = await pr.findOne(pid);
+
   // check that the product's quantity is 2
+  expect(updatedP!.quantity).toBe(2);
 });
 
 // Method: update
 // used when the client is editing a product they created
 it("Updates all the product fields changed by the client", async () => {
   // insert a product into the database
-  // create a product that has different field values
-  // call the update method
+  let p: ProductModel = {
+    name: "Comb",
+    price: 12.5,
+    quantity: 1,
+    description: "A basic comb for hair.",
+    category: ["hair care"],
+    imageURI: "/path/to/comb/image",
+    user: {
+      username: "Sears",
+      email: "Searsshipping@gmail.com",
+    },
+  };
+
+  let pid = await pr.create(p);
+
+  // update the product's field values
+  p.name = "Black Comb";
+  p.price = 13.2; // remember to make this a double when inserting into the db or we get a validation error
+  p.description = "A black comb for hair care.";
+
+  // update the product with the new field values
+  await pr.updateProduct(p, pid);
+
   // find the product in the database
-  // check that the updates have been made and that unchanged fields remain the same as that of the original product
+  let updatedProduct = await pr.findOne(pid);
+
+  // check that the fields changed by the client have been updated
+  expect(updatedProduct!.name).toBe(p.name);
+  expect(updatedProduct!.price).toBe(p.price);
+  expect(updatedProduct!.description).toBe(p.description);
+
+  // check that fields left unchanged by the client are still the same
+  expect(updatedProduct!.category).toBe(p.category);
+  expect(updatedProduct!.imageURI).toBe(p.imageURI);
+  expect(updatedProduct!.quantity).toBe(p.quantity);
+  expect(updatedProduct!.user.email).toBe(p.user.email);
+  expect(updatedProduct!.user.username).toBe(p.user.username);
 });
 
 // Method: delete
 // used when the client wants to delete a product they created
 it("Deletes a product by product id", async () => {
   // insert a product into the database
+  let p: ProductModel = {
+    name: "Comb",
+    price: 12.5,
+    quantity: 1,
+    description: "A basic comb for hair.",
+    category: ["hair care"],
+    imageURI: "/path/to/comb/image",
+    user: {
+      username: "Sears",
+      email: "Searsshipping@gmail.com",
+    },
+  };
+
+  let pid = await pr.create(p);
+
   // call the delete method
+  await pr.deleteProduct(pid);
+
   // attempt to retrieve the product
-  // ensure no product is found with that id
+  let deletedProduct = await pr.findOne(pid);
+
+  // ensure the returned product is undefined
+  expect(deletedProduct).toBe(undefined);
 });
 
 // Method: findByCategory
 // used by the client when they are searching for products by category
 it("Finds a set of products that match the category", async () => {
   // insert a set of products that have a category of "hair care"
-  // find the products using the repo's findByCategory method
-  // ensure that all of the inserted products are returned
-});
+  let p1: ProductModel = {
+    name: "Comb",
+    price: 12.5,
+    quantity: 1,
+    description: "A basic comb for hair.",
+    category: ["hair care"],
+    imageURI: "/path/to/comb/image",
+    user: {
+      username: "Sears",
+      email: "Searsshipping@gmail.com",
+    },
+  };
 
-// Method: findByCategory
-// used by the client when they are searching for products by category
-it("Finds a set of products that match the categories", async () => {
-  // insert a set of products that have a category of "hair care" or/and "clothing"
-  // search for products in the hair care and clothing categories using the repo's findByCategory method
+  let p2: ProductModel = {
+    name: "Black Hair Dye",
+    price: 10.0,
+    quantity: 1,
+    description: "Dye your hair black.",
+    category: ["hair care"],
+    imageURI: "/path/to/hair-dye/image",
+    user: {
+      username: "Sears",
+      email: "Searsshipping@gmail.com",
+    },
+  };
+
+  let p3: ProductModel = {
+    name: "Green Brush",
+    price: 7.0,
+    quantity: 1,
+    description: "A basic brush for hair.",
+    category: ["hair care"],
+    imageURI: "/path/to/green-brush/image",
+    user: {
+      username: "Sears",
+      email: "Searsshipping@gmail.com",
+    },
+  };
+
+  await pr.create(p1);
+  await pr.create(p2);
+  await pr.create(p3);
+
+  // find the products using the repo's findByCategory method
+  let products = await pr.findByCategory("hair care");
+
   // ensure that all of the inserted products are returned
+  expect(products.length).toBe(3);
+
+  // ensure all of the returned products have hair care as their category
+  expect(products[0].category).toBe("hair care");
+  expect(products[1].category).toBe("hair care");
+  expect(products[2].category).toBe("hair care");
 });
 
 // Method: findByName
@@ -157,14 +279,76 @@ it("Finds a set of products that match the categories", async () => {
 // IMP: Since this is using a simple text search index we will not receive "sneakers" results when searching for shoes, for instance.
 it("Finds products with a matching name or substring", async () => {
   // insert a set of products with "shoe" in their name
+  let p1: ProductModel = {
+    name: "Nike Shoes",
+    price: 12.5,
+    quantity: 1,
+    description: "A basic shoe for your feet.",
+    category: ["footwear"],
+    imageURI: "/path/to/comb/image",
+    user: {
+      username: "Sears",
+      email: "Searsshipping@gmail.com",
+    },
+  };
+
+  let p2: ProductModel = {
+    name: "Addidas Shoes",
+    price: 12.5,
+    quantity: 1,
+    description: "A basic shoe for your feet.",
+    category: ["footwear"],
+    imageURI: "/path/to/comb/image",
+    user: {
+      username: "Sears",
+      email: "Searsshipping@gmail.com",
+    },
+  };
+
+  let p3: ProductModel = {
+    name: "Shoes",
+    price: 12.5,
+    quantity: 1,
+    description: "A basic shoe for your feet.",
+    category: ["footwear"],
+    imageURI: "/path/to/comb/image",
+    user: {
+      username: "Sears",
+      email: "Searsshipping@gmail.com",
+    },
+  };
+
+  await pr.create(p1);
+  await pr.create(p2);
+  await pr.create(p3);
+
   // use the repo's findByName method to search for all products with shoe in the title
+  let products = await pr.findByName("shoes");
+
   // ensure that all of the inserted products are returned
+  expect(products.length).toBe(3);
 });
 
 // Method: findBySeller
 // used by the client when they are searching for products from a particular seller
 it("Finds all the products by a particular seller", async () => {
   // insert a set of products from seller "Viggo's Thrift"
+  let p: ProductModel = {
+    name: "Nike Shoes",
+    price: 12.5,
+    quantity: 1,
+    description: "A basic shoe for your feet.",
+    category: ["footwear"],
+    imageURI: "/path/to/comb/image",
+    user: {
+      username: "Viggo's Thrift",
+      email: "viggothrift@gmail.com",
+    },
+  };
+
   // search for viggo's thrift using findBySeller
+  let products = await pr.findBySeller("Viggo's Thrift");
+
   // ensure all of the inserted products are returned
+  expect(products.length).toBe(1);
 });
--- products/src/repos/products-repo.ts
+++ products/src/repos/products-repo.ts
@@ -1,7 +1,6 @@
 import { client } from "../client";
 import { ProductModel } from "../models/product-model";
 import { dbConfig } from "../config/database-config";
-import { createCipher } from "crypto";
 
 export class ProductsRepo {
   // get the products collection from the mongo client
@@ -25,23 +24,139 @@ export class ProductsRepo {
     }
   }
 
-  async findOne(id: string): Promise<ProductModel> {
+  async findOne(id: string): Promise<ProductModel | undefined> {
     try {
       // search for the product by id
-      let res = this.productsCollection!.findOne({ _id: id });
+      let res = await this.productsCollection!.findOne({ _id: id });
 
       // check if no product is found
-      //     return undefined
+      if (!res) {
+        // return undefined
+        return undefined;
+      }
 
-      // create an empty product model
-
-      // fill the product model fields with the result
+      // create the product model using the document data
+      let product: ProductModel = {
+        name: res.name,
+        price: res.price,
+        description: res.description,
+        imageURI: res.imageURI,
+        category: res.category,
+        quantity: res.quantity,
+        user: res.user,
+      };
 
       // return the product model to the client
+      return product;
     } catch (e) {
       throw new Error(e);
     }
   }
 
-  async orderCreated(id: string) {}
+  async findByCategory(category: string): Promise<Array<ProductModel>> {
+    return [
+      {
+        name: "Comb",
+        price: 12.5,
+        quantity: 1,
+        description: "A basic comb for hair.",
+        category: ["hair care"],
+        imageURI: "/path/to/comb/image",
+        user: {
+          username: "Sears",
+          email: "Searsshipping@gmail.com",
+        },
+      },
+    ];
+  }
+
+  async findByName(name: string): Promise<Array<ProductModel>> {
+    return [
+      {
+        name: "Comb",
+        price: 12.5,
+        quantity: 1,
+        description: "A basic comb for hair.",
+        category: ["hair care"],
+        imageURI: "/path/to/comb/image",
+        user: {
+          username: "Sears",
+          email: "Searsshipping@gmail.com",
+        },
+      },
+    ];
+  }
+
+  async findBySeller(sellerName: string): Promise<Array<ProductModel>> {
+    return [
+      {
+        name: "Comb",
+        price: 12.5,
+        quantity: 1,
+        description: "A basic comb for hair.",
+        category: ["hair care"],
+        imageURI: "/path/to/comb/image",
+        user: {
+          username: "Sears",
+          email: "Searsshipping@gmail.com",
+        },
+      },
+    ];
+  }
+
+  async updateProduct(updatedProduct: ProductModel, id: string) {
+    try {
+      console.log(updatedProduct);
+      // update the whole document using the passed in product
+      let res = await this.productsCollection!.updateOne(
+        { _id: id },
+        {
+          $set: {
+            name: updatedProduct.name,
+            price: updatedProduct.price,
+            description: updatedProduct.description,
+            category: updatedProduct.category,
+            quantity: updatedProduct.quantity,
+            imageURI: updatedProduct.imageURI,
+            user: updatedProduct.user,
+            // "user.email": updatedProduct.user.email,
+            // "user.username": updatedProduct.user.username,
+          },
+        }
+      );
+
+      // ensure the update was successful
+      if (!res.result.ok) {
+        throw new Error("Could not update the product");
+      }
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
+
+  async deleteProduct(id: string) {}
+
+  async changeQuantity(id: string, up: boolean) {
+    try {
+      let updateValue;
+      if (up) {
+        updateValue = 1;
+      } else {
+        updateValue = -1;
+      }
+
+      // find the product by its id and increment it
+      let document = await this.productsCollection!.updateOne(
+        { _id: id },
+        { $inc: { quantity: updateValue } }
+      );
+
+      // check the document to ensure it updated
+      if (!document.result.ok) {
+        throw new Error("Product not updated");
+      }
+    } catch (e) {
+      throw new Error(e);
+    }
+  }
 }
